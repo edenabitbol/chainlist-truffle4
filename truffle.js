f4445f6119fabbcf132ff32978f9988b8566b0c9
Eden Abitbol <edenjabitbol@gmail.com>
2019-10-28 22:44:12 -0400

    adding frontend files to Github pages

--- truffle.js
+++ truffle.js
@@ -8,10 +8,17 @@ module.exports = {
                network_id: "*" // Match any network id
           },
           chainskills: {
-            host: "localhost",
-            port: 8545,
-            network_id: "4224",
-            gas: 4700000
+               host: "localhost",
+               port: 8545,
+               network_id: "4224",
+               gas: 4700000
+          },
+          rinkeby: {
+               host: "localhost",
+               port: 8545,
+               network_id: "4", // official netwok id for rinkeby test net
+               gas: 4700000
           }
+
      }
 };
